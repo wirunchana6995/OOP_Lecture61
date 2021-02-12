430dafa41d8865a2404246237aa9194087bcbb5c
wirunchana <wirunchana.pair@gmail.com>
2021-02-12 23:14:19 +0700

    Add project OOP_Lecture61

---
+++ Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Customer
+    {
+        string name = "";
+        string lastName = "";
+        string address = ""; 
+        string email = "";
+
+        public string Name { get => name; set => name = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
